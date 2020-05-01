8956f122e27cf0785740302e89d68c79dc37f661
Cedric DENEUBOURG <deneubourg.cedric@thedigitalfab.com>
2020-05-01 02:25:30 +0200

    Add migrations

--- db/migrate/20200501001008_create_dogsitters.rb
+++ db/migrate/20200501001008_create_dogsitters.rb
@@ -1,6 +1,9 @@
 class CreateDogsitters < ActiveRecord::Migration[6.0]
   def change
     create_table :dogsitters do |t|
+      t.string :first_name
+      t.string :last_name
+      t.belongs_to :city, index: true
 
       t.timestamps
     end
